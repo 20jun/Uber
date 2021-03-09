b1c53aee55ea1bf5d3a8f69d6ada205cb0a674e4
sysner03 <sysner2@sysner2ui-Macmini.local>
2021-03-09 20:37:08 +0900

    Uber Clone App

---
+++ src/screens/HomeScreen/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import HomeMap from '../../components/HomeMap';
+import CovidMessage from '../../components/CovidMessage';
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <HomeMap />
+
+            <CovidMessage />
+
+            {/* Bottom Comp*/}
+        </View>
+    );
+};
+
+export default HomeScreen;
