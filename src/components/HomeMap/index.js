fa66cefc576714c364144e04bfc501b5bbd4bf40
sysner03 <sysner2@sysner2ui-Macmini.local>
2021-03-10 20:10:41 +0900

    2:53:22

--- src/components/HomeMap/index.js
+++ src/components/HomeMap/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 const HomeMap = () => {
     return (
@@ -9,7 +10,19 @@ const HomeMap = () => {
             justifyContent : 'center',
             alignItems : 'center',
         }}>
-            <Text>I am a map</Text>
+            <MapView
+            style = {{
+                width : '100%',
+                height : '100%',
+            }} 
+            provider = {PROVIDER_GOOGLE}
+            initialRegion = {{
+                latitude : 37.78825,
+                longitude : -122.4324,
+                latitudeDelta : 0.0922,
+                longitudeDelta : 0.0421,
+            }}
+            />
         </View>
     );
 };
--- src/components/UberTypes/index.js
+++ src/components/UberTypes/index.js
@@ -1,16 +1,35 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Pressable} from 'react-native';
 import styles from './styles';
 import UberTypeRow from '../UberTypeRow';
 
 import typesData from '../../assets/data/types';
 
 const UberTypes = () => {
+    const confirm = () => {
+        console.warn('confirm');
+    };
+
     return (
         <View>
             {typesData.map((type : {...} ) => (
             <UberTypeRow type = {type} />
             ))}
+
+
+            <Pressable onPress = {confirm} style ={{
+                backgroundColor : 'black',
+                padding : 10,
+                margin : 10,
+                alignItems : 'center',
+            }}>
+                <Text style = {{
+                    color : 'white',
+                    fontWeight : 'bold',
+                }}>
+                    Confirm Uber
+                </Text>
+            </Pressable>
         </View>
     );
 };
